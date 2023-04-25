a0c40101a18d02dbb98e269de6796c4efc9765c9
SandraKoch <sandraannakoch@gmail.com>
2023-04-25 21:46:31 +0200

    simplelightbox import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,10 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Import opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// const SimpleLightbox = window.SimpleLightbox;
+
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';

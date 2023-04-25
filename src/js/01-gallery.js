8b986301995e5051d865c0c2fb7fc3d53f8d1b0c
SandraKoch <sandraannakoch@gmail.com>
2023-04-25 23:10:21 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,34 @@ console.log(galleryItems);
 
 // Import opisany w dokumentacji
 import SimpleLightbox from 'simplelightbox';
-// const SimpleLightbox = window.SimpleLightbox;
+const SimpleLightbox = window.SimpleLightbox; // how to tell the difference
 
 // Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+let templates = galleryItems
+  .map(
+    item => `
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}" title="${item.description}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', templates);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});

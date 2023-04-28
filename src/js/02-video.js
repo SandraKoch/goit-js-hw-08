2e57c2436174b8b6954db4ebde1100fa1a6e70f9
SandraKoch <sandraannakoch@gmail.com>
2023-04-28 21:18:22 +0200

    task 2, local storage records and lodash throttle update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (video) {
+  console.log(video.seconds);
+  localStorage.setItem('videoplayer-current-time', video.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
